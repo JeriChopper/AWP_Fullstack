3ffc7e089b0a0bdb8a0fc05491f891b66963e1d6
JeriChopper <jerichokoskinen@hotmail.com>
2024-02-15 14:30:01 +0200

    main changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './components/Login';
+import Register from './components/Register';
 import './App.css';
 
 
@@ -8,7 +9,9 @@ function App() {
   return (
     <Router>
       <div className="App">
-      
+
+
+      <Register />
       <Login />
 
       </div>
